45ea63e25eb83c0b363fd7db2e532251ed585957
Fernelly Carvajal <fcarvajals@misena.edu.co>
2019-09-26 09:51:11 -0500

    Archivos modificados el 26 en la mañana

--- script1.js
+++ script1.js
@@ -3,3 +3,16 @@ data=JSON.parse(data);
 for(var i=0; i<data.length;i++){
     console.log(data[i].name+' is a '+data[i].race);
 }
+
+console.log("*************************************");
+var request=new XMLHttpRequest();
+request.open('GET','data.json',true);
+
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    for(var i=0;i<data.length;i++){
+        console.log(data[i].name+' es un '+data[i].race+'.');
+    }
+}
+
+request.send();
