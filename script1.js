24f453fb40a3ce6384cbfa8fb6a049caa4871d14
Fernelly Carvajal <fcarvajals@misena.edu.co>
2019-09-26 09:11:48 -0500

    Iniciando ejemplo 3

--- script1.js
+++ script1.js
@@ -1,1 +1,5 @@
 var data='[{"name":"Aragon","race":"Human"},{"name":"Gimli","race":"Dwarf"}]'
+data=JSON.parse(data);
+for(var i=0; i<data.length;i++){
+    console.log(data[i].name+' is a '+data[i].race);
+}
